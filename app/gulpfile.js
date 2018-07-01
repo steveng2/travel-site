b7d4133a982b627e794a9a4fc9245a00bcf07bb2
steven garner <stevengarner3@gmail.com>
2018-07-01 04:45:41 +0100

    posscss doesnt write to a temp file in the director of app

---
+++ app/gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer');
+
+gulp.task('default', function() {
+  console.log('Created a gulp task');
+});
+
+gulp.task('html', function() {
+  console.log('Something with HTML');
+});
+
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/styles/styles.css')
+  .pipe(postcss([autoprefixer]))
+  .pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', function() {
+  watch('./app/index.html', function() {
+    gulp.start('html');
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('styles');
+});
+
+});
